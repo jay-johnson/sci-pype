d9ea87f2bdba8dcb8e2ed2ac450728bbb4a533fd
jay-johnson <jay.p.h.johnson@gmail.com>
2017-01-30 21:06:29 -0800

    initial integration examples for powering the Machine Learning Data Store with Redis Lab's Online Cloud service (https://redislabs.com/redis-cloud)

---
+++ rl-stop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+filetouse="redis-labs-compose.yml"
+
+echo "Stopping Composition: $filetouse"
+docker-compose -f $filetouse stop
+echo "Done"
+
+docker stop redis-server jupyter >> /dev/null
+docker rm redis-server jupyter >> /dev/null
+
+exit 0
