683d7f6c7c2312d741725f9f6cf1bd837f52c1be
jay-johnson <jay.p.h.johnson@gmail.com>
2017-03-04 17:31:23 -0500

    fix for env file using the lookup dictionary

--- src/common/env_loader.py
+++ src/common/env_loader.py
@@ -45,49 +45,54 @@ def load_env_for_deployment():
                                                     }
                                 }
 
-            org_paths_to_test   = [
-                                    "../../../env/",
-                                    "../../env/",
-                                    "../env/",
-                                    "./env/"
-                                ]
-            env_paths_to_test   = os.getenv("ENV_SUPPORTED_ENV_PATHS", org_paths_to_test)
-            env_file            = path_to_envs + str(target_env_file)
-            if os.path.exists(env_file) == False:
-                for dir_test in env_paths_to_test:
-                    env_file    = str(dir_test) + str(target_env_file)
-                    if os.path.exists(env_file) == True:
-                        break
-            # end of loading the env inside or outside docker
+            if use_env in env_for_deploy:
+                
+                target_env_file     = str(env_for_deploy[use_env])
+                org_paths_to_test   = [
+                                        "../../../env/",
+                                        "../../env/",
+                                        "../env/",
+                                        "./env/"
+                                    ]
+                env_paths_to_test   = os.getenv("ENV_SUPPORTED_ENV_PATHS", org_paths_to_test)
+                env_file            = path_to_envs + str(target_env_file)
+                if os.path.exists(env_file) == False:
+                    for dir_test in env_paths_to_test:
+                        env_file    = str(dir_test) + str(target_env_file)
+                        if os.path.exists(env_file) == True:
+                            break
+                # end of loading the env inside or outside docker
 
-            if os.path.exists(env_file):
-                debug           = False
+                if os.path.exists(env_file):
+                    debug           = False
 
-                if debug:
-                    print ""
-                    print "Manually assigning Environment variables from file: " + str(env_file)
-                    print ""
+                    if debug:
+                        print ""
+                        print "Manually assigning Environment variables from file: " + str(env_file)
+                        print ""
 
-                for raw_line in open(env_file).readlines():
-                    line        = str(raw_line).replace("\n", "")
-                    if "#" not in str(line) and "=" in str(line):
-                        splt_ar = line.split("=")
-                        if len(splt_ar) > 0:
-                            key     = splt_ar[0]
-                            if str(key) != "ENV_IN_DOCKER":
-                                value   = "=".join(splt_ar[1:])
-                                os.environ[str(key)] = value
+                    for raw_line in open(env_file).readlines():
+                        line        = str(raw_line).replace("\n", "")
+                        if "#" not in str(line) and "=" in str(line):
+                            splt_ar = line.split("=")
+                            if len(splt_ar) > 0:
+                                key     = splt_ar[0]
+                                if str(key) != "ENV_IN_DOCKER":
+                                    value   = "=".join(splt_ar[1:])
+                                    os.environ[str(key)] = value
 
-                                if debug:
-                                    print str(key) + " => " + str(value)
-                        # end of if the file has 
-                    # end of checking if the line has an # and = characters
-                # end of for all lines in the env file to load in this is not inside docker
+                                    if debug:
+                                        print str(key) + " => " + str(value)
+                            # end of if the file has 
+                        # end of checking if the line has an # and = characters
+                    # end of for all lines in the env file to load in this is not inside docker
 
-                if debug:
-                    print ""
+                    if debug:
+                        print ""
+                else:
+                    print "Did not find the Environment file: " + str(env_file)
             else:
-                print "Did not find the Environment file: " + str(env_file)
+                print "Not a supported Environment(" + str(use_env) + "): " + str(env_file)
             # end of loading env in case this is not inside docker 
         else:
             print "Not have a valid deployment type:" + str(use_env)
